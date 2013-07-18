f130839d4238e261662aaaf6ce3cb1ba8431f52e
Kurt Garloff <kurt@garloff.de>
2013-07-18 08:08:31 +0000

    While being at it, add an exact version using popcnt ...

--- find_nonzero.c
+++ find_nonzero.c
@@ -18,7 +18,7 @@
 	for (i = 0; i < rep; ++i)	\
 		ln = routine(buf, SIZE);\
 	gettimeofday(&t2, NULL);	\
-	printf("%5i x %18s (%8i): %.3fs => %8i\n",\
+	printf("%5i x %24s (%8i): %.3fs => %8i\n",\
 		rep, #routine, sz,	\
 		t2.tv_sec-t1.tv_sec+0.000001*(t2.tv_usec-t1.tv_usec), ln)
 
@@ -29,6 +29,12 @@
 #define TEST_SIMD(a,b,c) do {} while (0)
 #endif
 
+#if defined(HAVE_SIMD_EXACT)
+#define TEST_SIMD_EXACT(a,b,c) TEST(a,b,c)
+#else
+#define TEST_SIMD_EXACT(a,b,c) do {} while (0)
+#endif
+
 int main()
 {
 	unsigned char* buf = (unsigned char*)malloc(SIZE);
@@ -37,20 +43,27 @@ int main()
 	memset(buf, 0xa5, SIZE);
 	TEST(1024*1024, find_nonzero_c, 2048);
 	TEST_SIMD(1024*1024, find_nonzero_simd, 2048);
+	TEST_SIMD_EXACT(1024*1024, find_nonzero_simd_exact, 2048);
 	TEST(1024*1024+3, find_nonzero_c, 2048);
 	TEST_SIMD(1024*1024+3, find_nonzero_simd, 2048);
+	TEST_SIMD_EXACT(1024*1024+3, find_nonzero_simd_exact, 2048);
 	TEST(1024*1024+6, find_nonzero_c, 2048);
 	TEST_SIMD(1024*1024+6, find_nonzero_simd, 2048);
+	TEST_SIMD_EXACT(1024*1024+6, find_nonzero_simd_exact, 2048);
 	TEST(1024*1024+9, find_nonzero_c, 2048);
 	TEST_SIMD(1024*1024+9, find_nonzero_simd, 2048);
+	TEST_SIMD_EXACT(1024*1024+9, find_nonzero_simd_exact, 2048);
 	TEST(1024*1024+16, find_nonzero_c, 2048);
 	TEST_SIMD(1024*1024+16, find_nonzero_simd, 2048);
+	TEST_SIMD_EXACT(1024*1024+16, find_nonzero_simd_exact, 2048);
 
 	TEST(16*1024*1024, find_nonzero_c, 128);
 	TEST_SIMD(16*1024*1024, find_nonzero_simd, 128);
+	TEST_SIMD_EXACT(16*1024*1024, find_nonzero_simd_exact, 128);
 
 	TEST(64*1024*1024, find_nonzero_c, 32);
 	TEST_SIMD(64*1024*1024, find_nonzero_simd, 32);
+	TEST_SIMD_EXACT(64*1024*1024, find_nonzero_simd_exact, 32);
 
 	free(buf);
 	return 0;
--- find_nonzero.h
+++ find_nonzero.h
@@ -7,6 +7,13 @@
 #define _FIND_NONZERO_H
 #include <sys/types.h>
 
+
+#ifndef NOINLINE
+# define STATIC_INLINE static inline
+#else
+# define STATIC_INLINE
+#endif
+
 /* FIXME: Is there no library function to find the first non-null byte?
  * Something like ffs()?
  * Here is an optimized version using SSE2 intrinsics, but there should be
@@ -18,10 +25,10 @@
 
 #define find_nonzero find_nonzero_simd
 /* This has been inspired by http://developer.amd.com/community/blog/faster-string-operations/ */
-static inline size_t find_nonzero_simd(const unsigned char* blk, const size_t ln)
+STATIC_INLINE size_t find_nonzero_simd(const unsigned char* blk, const size_t ln)
 {
 	__m128i xmm, zero = _mm_setzero_si128();
-	long register rax;
+	unsigned long register rax;
 	size_t i;
 	for (i = 0; i < ln; i+= 16) {
 		xmm = _mm_load_si128((__m128i*)(blk+i));
@@ -33,11 +40,30 @@ static inline size_t find_nonzero_simd(const unsigned char* blk, const size_t ln
 	return i;
 }
 
+#ifdef __SSE4_2__
+#define HAVE_SIMD_EXACT
+#include <smmintrin.h>
+STATIC_INLINE size_t find_nonzero_simd_exact(const unsigned char* blk, const size_t ln)
+{
+	__m128i xmm, zero = _mm_setzero_si128();
+	unsigned long register rax;
+	size_t i;
+	for (i = 0; i < ln; i+= 16) {
+		xmm = _mm_load_si128((__m128i*)(blk+i));
+		_mm_cmpeq_epi8(xmm, zero);
+		rax = _mm_movemask_epi8(xmm);
+		if (rax) 
+			return i + _mm_popcnt_u32(rax^(~(-rax))) - 1;
+	}
+	return ln;
+}
+#endif
+
 #else
 #define find_nonzero find_nonzero_c
 #endif
 /** return length of zero bytes, rounded to sizeof(long) */
-static inline size_t find_nonzero_c(const unsigned char* blk, const size_t ln)
+STATIC_INLINE size_t find_nonzero_c(const unsigned char* blk, const size_t ln)
 {
 	const unsigned long* ptr = (const unsigned long*)blk;
 	const unsigned long* const bptr = ptr;
