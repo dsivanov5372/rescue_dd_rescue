7c89eaa36c04ab7e4c22d3d5dfc5d6e5f59c6cf4
Kurt Garloff <kurt@garloff.de>
2013-07-18 12:19:13 +0000

    Allow cmd line arg to scale repetitions

--- find_nonzero.c
+++ find_nonzero.c
@@ -22,7 +22,7 @@
 	}				\
 	gettimeofday(&t2, NULL);	\
 	tdiff = t2.tv_sec-t1.tv_sec + 0.000001*(t2.tv_usec-t1.tv_usec);	\
-	printf("%6i x %24s (%8i): %8i (%.3fs => %5.0fMB/s)\n",	\
+	printf("%7i x %18s (%8i): %8i (%6.3fs => %5.0fMB/s)\n",	\
 		rep, #routine, sz, ln, tdiff, (double)(rep)*(double)(sz)/(1024*1024*tdiff))
 
 
@@ -32,29 +32,32 @@
 #define TEST_SIMD(a,b,c) do {} while (0)
 #endif
 
-int main()
+int main(int argc, char* argv[])
 {
 	unsigned char* obuf = (unsigned char*)malloc(SIZE+15);
 	unsigned char* buf = obuf+15;
 	struct timeval t1, t2;
-	int i, ln;
+	int i, ln = 0;
 	double tdiff;
+	int scale = 16;
+	if (argc > 1)
+		scale = atoi(argv[1]);
 	buf -= (unsigned long)buf%16;
 	memset(buf, 0xa5, SIZE);
-	TEST(8*1024-15, find_nonzero_c, 1024*256);
-	TEST_SIMD(8*1024-15, find_nonzero_simd, 1024*256);
-	TEST(32*1024-9, find_nonzero_c, 1024*64);
-	TEST_SIMD(32*1024-9, find_nonzero_simd, 1024*64);
-	TEST(128*1024-8, find_nonzero_c, 1024*16);
-	TEST_SIMD(128*1024-8, find_nonzero_simd, 1024*16);
-	TEST(1024*1024-7, find_nonzero_c, 2048);
-	TEST_SIMD(1024*1024-7, find_nonzero_simd, 2048);
-	TEST(4096*1024-1, find_nonzero_c, 512);
-	TEST_SIMD(4096*1024-1, find_nonzero_simd, 512);
-	TEST(16*1024*1024, find_nonzero_c, 128);
-	TEST_SIMD(16*1024*1024, find_nonzero_simd, 128);
-	TEST(64*1024*1024, find_nonzero_c, 32);
-	TEST_SIMD(64*1024*1024, find_nonzero_simd, 32);
+	TEST(8*1024-15, find_nonzero_c, 1024*256*scale/16);
+	TEST_SIMD(8*1024-15, find_nonzero_simd, 1024*256*scale/16);
+	TEST(32*1024-9, find_nonzero_c, 1024*64*scale/16);
+	TEST_SIMD(32*1024-9, find_nonzero_simd, 1024*64*scale/16);
+	TEST(128*1024-8, find_nonzero_c, 1024*16*scale/16);
+	TEST_SIMD(128*1024-8, find_nonzero_simd, 1024*16*scale/16);
+	TEST(1024*1024-7, find_nonzero_c, 2048*scale/16);
+	TEST_SIMD(1024*1024-7, find_nonzero_simd, 2048*scale/16);
+	TEST(4096*1024-1, find_nonzero_c, 512*scale/16);
+	TEST_SIMD(4096*1024-1, find_nonzero_simd, 512*scale/16);
+	TEST(16*1024*1024, find_nonzero_c, 128*scale/16);
+	TEST_SIMD(16*1024*1024, find_nonzero_simd, 128*scale/16);
+	TEST(64*1024*1024, find_nonzero_c, 32*scale/16);
+	TEST_SIMD(64*1024*1024, find_nonzero_simd, 32*scale/16);
 
 	free(obuf);
 	return 0;
