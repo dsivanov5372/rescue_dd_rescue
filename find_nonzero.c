49dbc249b6cb155598a94abaa5481453c994343c
Kurt Garloff <kurt@garloff.de>
2013-08-06 08:39:05 +0000

    Use two more registers and better interleave insns on ARM (+10%).

--- find_nonzero.c
+++ find_nonzero.c
@@ -116,46 +116,52 @@ size_t find_nonzero_arm6(const unsigned char *blk, const size_t ln)
 	"	ldmia %0!,{r2,r3}	\n"
 	"	cmp r2, #0		\n"
 	"	bne 2f			\n"
+	"	ldmia %0!,{r4,r5}	\n"
 	"	cmp r3, #0		\n"
 	"	bne 3f			\n"
-	"	ldmia %0!,{r2, r3}	\n"
-	"	cmp r2, #0		\n"
-	"	bne 2f			\n"
-	"	cmp r3, #0		\n"
-	"	bne 3f			\n"
+	"	cmp r4, #0		\n"
+	"	bne 4f			\n"
+	"	cmp r5, #0		\n"
+	"	bne 5f			\n"
 	"	cmp %0, %2		\n"	/* end? */
 	"	blt 1b			\n"
 	"	mov %0, %2		\n"	
 	"	b 10f			\n"	/* exhausted search */
 	"2:				\n"
-	"	sub %0, #4		\n"	/* First u32 is non-zero */
+	"	add %0, #4		\n"	/* First u32 is non-zero */
 	"	mov r3, r2		\n"
 	"3:				\n"
 	"	sub %0, #4		\n"
+	"	mov r4, r3		\n"
+	"4:				\n"
+	"	sub %0, #4		\n"
+	"	mov r5, r4		\n"
+	"5:				\n"
+	"	sub %0, #4		\n"
 //#ifndef __ARMEB__				/* Little endian bitmasks */
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-	"	tst r3, #0xff		\n"
+	"	tst r5, #0xff		\n"
 	"	bne 10f			\n"
 	"	add %0, #1		\n"
-	"	tst r3, #0xff00		\n"
+	"	tst r5, #0xff00		\n"
 	"	bne 10f			\n"
 	"	add %0, #1		\n"
-	"	tst r3, #0xff0000	\n"
+	"	tst r5, #0xff0000	\n"
 #else
-	"	tst r3, #0xff000000	\n"
+	"	tst r5, #0xff000000	\n"
 	"	bne 10f			\n"
 	"	add %0, #1		\n"
-	"	tst r3, #0xff0000	\n"
+	"	tst r5, #0xff0000	\n"
 	"	bne 10f			\n"
 	"	add %0, #1		\n"
-	"	tst r3, #0xff00		\n"
+	"	tst r5, #0xff00		\n"
 #endif
 	"	bne 10f			\n"
 	"	add %0, #1		\n"	
 	"10:				\n"
 	: "=r"(res)
 	: "0"(blk), "r"(end)
-	: "r2", "r3");
+	: "r2", "r3", "r4", "r5");
 	return res-blk;
 }
 #define find_nonzero_simd find_nonzero_arm6
