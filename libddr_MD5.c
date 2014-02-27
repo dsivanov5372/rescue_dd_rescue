7f5da0c6dce68621352116eaaf63c513c07b726b
Kurt Garloff <kurt@garloff.de>
2014-02-27 15:29:18 +0000

    It compiles!

--- libddr_MD5.c
+++ libddr_MD5.c
@@ -12,9 +12,14 @@
 
 #include <stdlib.h>
 
+/* fwd decl */
+ddr_plugin_t plug;
+
 typedef struct _md5_state {
 	md5_ctx md5;
 	loff_t first_ooff;
+	const char* onm;
+	char closed;
 } md5_state;
 
 int md5_open(int ifd, const char* inm, loff_t ioff, 
@@ -25,9 +30,52 @@ int md5_open(int ifd, const char* inm, loff_t ioff,
 	md5_state *state = (md5_state*)malloc(sizeof(md5_state));
 	*stat = (void*)state;
 	state->first_ooff = ooff;
+	if (ooff % 64)
+		plug.fplog(stderr, WARN, "First block not 64byte aligned, will break\n");
+	state->closed = 0;
+	state->onm = onm;
 	md5_init(&state->md5);
 	return 0;
 }
+
+unsigned char* md5_block(unsigned char* bf, int *towr, 
+			 loff_t ooff, void **stat)
+{
+	md5_state *state = (md5_state*)*stat;
+	/* The initial offset should be 64byte aligned ... */
+	if (*towr % 64) {
+		md5_calc(bf, *towr, (ooff+*towr-state->first_ooff), &state->md5);
+		state->closed = 1;
+	} else
+		md5_calc(bf, *towr, 0, &state->md5);
+	return bf;
+}
+
+static char _md5_out_str[36];
+char* md5_out(uint8_t* res)
+{
+	int i;
+    	for (i = 0; i < 16; i++)
+        	sprintf(_md5_out_str+2*i, "%2.2x", res[i]);
+	return _md5_out_str;
+}
+
+int md5_close(loff_t ooff, void **stat)
+{
+	md5_state *state = (md5_state*)*stat;
+	if (!state->closed) {
+		uint8_t bf[64];
+		md5_calc(bf, 0, ooff-state->first_ooff, &state->md5);
+		state->closed = 1;
+	}
+	uint8_t res[16];
+	md5_result(&state->md5, res);
+	plug.fplog(stderr, INFO, "md5sum %s (%i bytes): %s\n",
+		state->onm, ooff-state->first_ooff, md5_out(res));
+	free(*stat);
+	return 0;
+}
+
 
 ddr_plugin_t plug = {
 	.name = "MD5",
