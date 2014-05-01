e17f8efbd78c7e9a9cd0b52f6815a8eb682698fa
Kurt Garloff <kurt@garloff.de>
2014-05-01 16:02:30 +0000

    Add fuzzer (still empty ...)

---
+++ break_lzo.c
@@ -0,0 +1,16 @@
+/** \file break_lzo.c
+ * This program produces broken LZO files to test robustness
+ * of the libddr_lzo decompressor against broken files
+ * as this is a potential attack vector for malicious folks.
+ *
+ * Overwriting random bytes is simple, but we can also be more
+ * clever in fuzzing and fix checksums to see whether we can expose
+ * vulnerabilities this way.
+ *
+ * (c) Kurt Garloff <kurt@garloff.de>, 5/2014
+ * License: GNU GPLv2 or v3.
+ */
+
+#include <lzo/lzo1x.h>
+#include <stdio.h>
+
