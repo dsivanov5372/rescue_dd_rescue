7181ec1de97eccd1812a265469dba926c2b72e65
Kurt Garloff <kurt@garloff.de>
2014-05-19 21:09:54 +0000

    Allow return code range in fuzz testing.

--- test_lzo_fuzz.sh
+++ test_lzo_fuzz.sh
@@ -5,14 +5,15 @@ LZOP=`type -p lzop || echo /usr/bin/true`
 test_fuzz()
 {
 	ERC=$1
-	PAR=$2
-	EDIFF=$3
-	shift; shift; shift
+	ERCM=$2
+	PAR=$3
+	EDIFF=$4
+	shift 4
 	./fuzz_lzo $* dd_rescue dd_rescue.lzo || exit 1
 	echo ./dd_rescue -tL ./libddr_lzo.so$PAR dd_rescue.lzo dd_rescue.cmp
 	./dd_rescue -tL ./libddr_lzo.so$PAR dd_rescue.lzo dd_rescue.cmp
 	RC=$?
-	if test $RC != $ERC; then echo "Unexpected exit value $RC (exp: $ERC)"; exit 2; fi
+	if test $RC -lt $ERC -o $RC -gt $ERCM; then echo "Unexpected exit value $RC (exp: $ERC-$ERCM)"; exit 2; fi
 	echo "Exit code $RC, good"
 	echo -n "# of differences: "
        	DIFF=`cmp -l dd_rescue dd_rescue.cmp | wc -l`
@@ -26,14 +27,14 @@ $LZOP -vl dd_rescue.lzo
 ./dd_rescue -L ./libddr_lzo.so dd_rescue.lzo /dev/null
 
 # Main tests ...
-test_fuzz 0 "" 0 -m3
-test_fuzz 1 "" 16384 -U2
-test_fuzz 1 "=nodiscard" 0 -U2
-test_fuzz 1 "=nodiscard" 0 -C3
-test_fuzz 1 "" 16384 -x1:0x6fe=0x1a
-test_fuzz 1 "=nodiscard" 32 -x1:0x6fe=0x1a
-test_fuzz 1 "" 16384 -u2=8192
-#test_fuzz 2 "" 100000 -c4=8192		# disabled because exit code can be either 1 or 2 ...
+test_fuzz 0 0 "" 0 -m3
+test_fuzz 1 1 "" 16384 -U2
+test_fuzz 1 1 "=nodiscard" 0 -U2
+test_fuzz 1 1 "=nodiscard" 0 -C3
+test_fuzz 1 5 "" 16384 -x1:0x6fe=0x1a
+test_fuzz 1 5 "=nodiscard" 32 -x1:0x6fe=0x1a
+test_fuzz 1 3 "" 16384 -u2=8192
+test_fuzz 1 4 "" 100000 -c4=8192
 # TODO: A lot more tests, with and without nodiscard
 # TODO: Do tests with -T, with good preexisting data and check whether nothing gets destroyed
 rm -f dd_rescue.lzo dd_rescue.cmp
