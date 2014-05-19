f22262608be7c6229c42782f575169973d35a3c3
Kurt Garloff <kurt@garloff.de>
2014-05-19 13:39:12 +0000

    Refactor sparse handling in MD5 plugin.

--- libddr_MD5.c
+++ libddr_MD5.c
@@ -86,115 +86,10 @@ int md5_open(const opt_t *opt, int olnchg,
 	state->olnchg = olnchg;
 	if (olnchg && (state->opts->sparse || !state->opts->nosparse)) {
 		FPLOG(WARN, "Size of potential holes may not be correct due to subsequent plugins;\n");
-		FPLOG(WARN, " MD5 hash may be miscomputed! Avoid holes (remove -a, specify -A).\n");
+		FPLOG(WARN, " MD5 hash may be miscomputed! Avoid holes (remove -a, use -A).\n");
 	}
 	return 0;
 }
-
-void md5_last(md5_state *state, loff_t ooff)
-{
-	//md5_block(0, 0, ooff, stat);
-	loff_t len = ooff - state->opts->init_opos;
-	int left = len - state->md5_pos;
-	assert(state->olnchg || state->buflen == left);
-	/*
-	fprintf(stderr, "MD5_DEBUG: %s: len=%li, md5pos=%li\n", 
-		state->name, len, state->md5_pos);
-	 */
-	MD5_DEBUG(FPLOG(INFO, "Last block with %i bytes\n", state->buflen));
-	md5_calc(state->buf, state->buflen, state->md5_pos+state->buflen, &state->md5);
-	state->md5_pos += state->buflen;
-}
-
-#define MIN(a,b) ((a)<(b)? (a): (b))
-
-/* This is rather complex, as we handle both non-aligned first block size
- * as well as sparse files */
-unsigned char* md5_block(fstate_t *fst, unsigned char* bf, 
-			 int *towr, int eof, int *recall, void **stat)
-{
-	md5_state *state = (md5_state*)*stat;
-	const loff_t ooff = fst->opos;
-	const loff_t opos = state->olnchg? fst->ipos-state->opts->init_ipos : ooff-state->opts->init_opos;
-	int consumed = 0;
-	assert(bf);
-	MD5_DEBUG(FPLOG(INFO, "block(%i/%i): towr=%i, eof=%i, ooff=%i, md5_pos=%i, buflen=%i\n",
-				state->seq, state->olnchg, *towr, eof, ooff, state->md5_pos, state->buflen));
-	/* TODO: Replace usage of state->buf by using slack space */
-	/* First block */
-	if (state->buflen) {
-		/* Handle leftover bytes ... */
-		if (/*!state->olnchg &&*/ opos > state->md5_pos+state->buflen) {
-			/* First sparse piece  ... */
-			memset(state->buf+state->buflen, 0, 64-state->buflen);
-			md5_64(state->buf, &state->md5);
-			state->md5_pos += 64;
-			memset(state->buf, 0, state->buflen);
-			state->buflen = 0;
-		} else if (*towr) {
-			/* Reassemble and process first block */
-			consumed = MIN(64-state->buflen, *towr);
-			MD5_DEBUG(FPLOG(INFO, "Append %i bytes @ %i to store\n", consumed, ooff));
-			memcpy(state->buf+state->buflen, bf, consumed);
-			if (consumed+state->buflen == 64) {
-				md5_64(state->buf, &state->md5);
-				state->md5_pos += 64;
-				memset(state->buf, 0, 64);
-				state->buflen = 0;
-			} else 
-				state->buflen += consumed;
-		}
-	}
-	assert(state->olnchg || state->md5_pos <= opos + consumed);
-	/* Bulk sparse process */
-	while (/*!state->olnchg &&*/ opos > state->md5_pos+63) {
-		assert(state->buflen == 0);
-		md5_64(state->buf, &state->md5);
-		state->md5_pos += 64;
-	}
-	/* Last sparse block */
-	int left = opos - (state->md5_pos+state->buflen);
-	if (left > 0 && *towr == 0 && eof) {
-		memset(state->buf, 0, left);
-		state->buflen = left;
-		md5_last(state, ooff);
-		return bf;
-	}
-	if (/*!state->olnchg &&*/ left > 0 && *towr >= left) {
-		assert(consumed == 0);
-		memcpy(state->buf+64-left, bf, left);
-		md5_64(state->buf, &state->md5);
-		state->md5_pos += 64;
-		state->buflen = 0;
-		consumed = left;
-		memset(state->buf+64-left, 0, left);
-	}
-	/* Bulk buffer process */
-	int mylen = *towr - consumed; 
-	assert(mylen >= 0);
-	mylen -= mylen%64;
-	if (mylen) {
-		MD5_DEBUG(FPLOG(INFO, "Consume %i bytes @ %i\n", mylen, ooff+consumed));
-		md5_calc(bf+consumed, mylen, 0, &state->md5);
-		consumed += mylen; state->md5_pos += mylen;
-	}
-	/* Copy remainder into buffer */
-	if (/*!state->olnchg &&*/ state->md5_pos + state->buflen != opos + consumed)
-		FPLOG(FATAL, "Inconsistency: MD5 pos %i, buff %i, st pos %i, cons %i, tbw %i\n",
-				state->md5_pos, state->buflen, opos, consumed, *towr);
-	assert(state->olnchg || eof || state->md5_pos + state->buflen == opos + consumed);
-	if (*towr - consumed) {
-		assert(state->buflen+*towr-consumed < 64);
-		MD5_DEBUG(FPLOG(INFO, "Store %i bytes @ %i\n", *towr-consumed, ooff+consumed));
-		memcpy(state->buf+state->buflen, bf+consumed, *towr-consumed);
-		state->buflen += *towr-consumed;
-	}
-	if (eof)
-		md5_last(state, ooff+*towr);
-	return bf;
-}
-
-
 
 #if __WORDSIZE == 64
 #define LL "l"
@@ -203,6 +98,124 @@ unsigned char* md5_block(fstate_t *fst, unsigned char* bf,
 #else
 #error __WORDSIZE unknown
 #endif
+
+void md5_last(md5_state *state, loff_t pos)
+{
+	//md5_block(0, 0, ooff, stat);
+	int left = pos - state->md5_pos;
+	assert(state->buflen == left);
+	/*
+	fprintf(stderr, "MD5_DEBUG: %s: len=%li, md5pos=%li\n", 
+		state->name, len, state->md5_pos);
+	 */
+	MD5_DEBUG(FPLOG(DEBUG, "Last block with %i bytes\n", state->buflen));
+	md5_calc(state->buf, state->buflen, pos, &state->md5);
+	state->md5_pos += state->buflen;
+}
+
+static inline void md5_64_buf(md5_state* state, int clear)
+{
+	md5_64(state->buf, &state->md5);
+	state->md5_pos += 64;
+	state->buflen = 0;
+	if (clear)
+		memset(state->buf, 0, clear);
+}
+
+#define MIN(a,b) ((a)<(b)? (a): (b))
+
+void md5_hole(fstate_t *fst, md5_state *state, loff_t holelen)
+{
+	if (state->buflen) {
+		MD5_DEBUG(FPLOG(DEBUG, "first sparse block (drain %i)\n", state->buflen));
+		memset(state->buf+state->buflen, 0, 64-state->buflen);
+		if (holelen >= 64-state->buflen) {
+			holelen -= (64-state->buflen);
+			md5_64_buf(state, state->buflen);
+		} else {
+			state->buflen += holelen;
+			return;
+		}
+	}
+	assert(state->buflen == 0);
+	MD5_DEBUG(FPLOG(DEBUG, "bulk sparse %i\n", holelen-holelen%64));
+	while (holelen >= 64) {
+		md5_64_buf(state, 0);
+		holelen -= 64;
+	}
+	assert(holelen >= 0 && holelen < 64);
+	// memset(state->buf, 0, holelen);
+	state->buflen = holelen;
+	MD5_DEBUG(FPLOG(DEBUG, "sparse left %i (%i+%i)\n", holelen, state->md5_pos, state->buflen));
+	return;
+}
+
+/* This is rather complex, as we handle both non-aligned first block size
+ * as well as sparse files */
+unsigned char* md5_block(fstate_t *fst, unsigned char* bf, 
+			 int *towr, int eof, int *recall, void **stat)
+{
+	/* TODO: Replace usage of state->buf by using slack space
+	 * Hmmm, really? Probably buffer management is not sophisticated enough currently ... */
+	md5_state *state = (md5_state*)*stat;
+	const loff_t pos = state->olnchg? 
+			fst->ipos - state->opts->init_ipos:
+			fst->opos - state->opts->init_opos;
+	MD5_DEBUG(FPLOG(DEBUG, "block(%i/%i): towr=%i, eof=%i, pos=%" LL "i, md5_pos=%" LL "i, buflen=%i\n",
+				state->seq, state->olnchg, *towr, eof, pos, state->md5_pos, state->buflen));
+	// Handle hole (sparse files)
+	const loff_t holesz = pos - (state->md5_pos + state->buflen);
+	assert(holesz >= 0);
+	if (holesz)
+		md5_hole(fst, state, holesz);
+
+	assert(pos == state->md5_pos+state->buflen);
+	int consumed = 0;
+	assert(bf);
+	/* First block */
+	if (state->buflen && *towr) {
+		/* Reassemble and process first block */
+		consumed = MIN(64-state->buflen, *towr);
+		MD5_DEBUG(FPLOG(DEBUG, "Append %i bytes @ %i to store\n", consumed, pos));
+		memcpy(state->buf+state->buflen, bf, consumed);
+		if (consumed+state->buflen == 64) {
+			md5_64_buf(state, 64);
+		} else {
+			state->buflen += consumed;
+			//memset(state->buf+state->buflen, 0, 64-state->buflen);
+		}
+	}
+
+	assert(state->md5_pos+state->buflen == pos+consumed);
+	/* Bulk buffer process */
+	int to_process = *towr - consumed;
+	assert(to_process >= 0);
+	to_process -= to_process%64;
+	if (to_process) {
+		MD5_DEBUG(FPLOG(DEBUG, "Consume %i bytes @ %i\n", to_process, pos+consumed));
+		assert(state->buflen == 0);
+		md5_calc(bf+consumed, to_process, 0, &state->md5);
+		consumed += to_process; state->md5_pos += to_process;
+	}
+	assert(state->md5_pos+state->buflen == pos+consumed);
+	to_process = *towr - consumed;
+	assert(to_process >= 0 && to_process < 64);
+	/* Copy remainder into buffer */
+	if (/*!state->olnchg &&*/ state->md5_pos + state->buflen != pos + consumed)
+		FPLOG(FATAL, "Inconsistency: MD5 pos %i, buff %i, st pos %" LL "i, cons %i, tbw %i\n",
+				state->md5_pos, state->buflen, pos, consumed, *towr);
+	if (to_process) {
+		MD5_DEBUG(FPLOG(DEBUG, "Store %i bytes @ %" LL "i\n", to_process, pos+consumed));
+		assert(state->buflen == 0);
+		memcpy(state->buf+state->buflen, bf+consumed, to_process);
+		state->buflen = to_process;
+	}
+	if (eof)
+		md5_last(state, pos);
+	return bf;
+}
+
+
 
 static char _md5_out_str[36];
 char* md5_out(uint8_t* res)
