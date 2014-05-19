10c957d28057c30b0b265665240f0f54517f7f59
Kurt Garloff <kurt@garloff.de>
2014-05-19 17:04:04 +0000

    Add some more fuzz testing ...

--- test_lzo_fuzz.sh
+++ test_lzo_fuzz.sh
@@ -4,12 +4,18 @@ test_fuzz()
 {
 	ERC=$1
 	PAR=$2
-	shift; shift
+	EDIFF=$3
+	shift; shift; shift
 	./fuzz_lzo $* dd_rescue dd_rescue.lzo || exit 1
-	./dd_rescue -TL ./libddr_lzo.so$PAR dd_rescue.lzo dd_rescue.cmp
+	echo ./dd_rescue -TL ./libddr_lzo.so$PAR dd_rescue.lzo dd_rescue.cmp
+	./dd_rescue -tL ./libddr_lzo.so$PAR dd_rescue.lzo dd_rescue.cmp
 	RC=$?
 	if test $RC != $ERC; then echo "Unexpected exit value $RC (exp: $ERC)"; exit 2; fi
 	echo "Exit code $RC, good"
+	echo -n "# of differences: "
+       	DIFF=`cmp -l dd_rescue dd_rescue.cmp | wc -l`
+	echo $DIFF
+	if test "$DIFF" -gt "$EDIFF"; then echo "More differences than expected ..."; exit 3; fi
 }
 
 
@@ -18,8 +24,11 @@ lzop -vl dd_rescue.lzo
 ./dd_rescue -L ./libddr_lzo.so dd_rescue.lzo /dev/null
 
 # Main tests ...
-test_fuzz 0 "" -m3
-test_fuzz 1 "" -U2
-test_fuzz 1 "=nodiscard" -U2
+test_fuzz 0 "" 0 -m3
+test_fuzz 1 "" 16384 -U2
+test_fuzz 1 "=nodiscard" 0 -U2
+test_fuzz 1 "=nodiscard" 0 -C3
+test_fuzz 1 "" 16384 -x1:0x6fe=0x1a
+test_fuzz 1 "=nodiscard" 32 -x1:0x6fe=0x1a
 
 
