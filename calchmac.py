5ce155b338beec9f49c2ff53b58b890523de4265
Kurt Garloff <kurt@garloff.de>
2014-08-06 19:40:34 +0000

    Create HMACS.alg file and allow user to chose algo.

--- calchmac.py
+++ calchmac.py
@@ -3,16 +3,40 @@ import hashlib
 import hmac
 import sys
 
-if len(sys.argv) < 3:
-	print >>sys.stderr, "Usage: calchmac.py PASS SALT"
+if len(sys.argv) < 4:
+	print >>sys.stderr, "Usage: calchmac.py ALG PASS FILE [FILE [..]]"
 	sys.exit(1)
 
-pwd = sys.argv[1]
-salt = sys.argv[2]
-salt1 = salt + "\0\0\0\x01"
+algtbl = (("md5", hashlib.md5),
+	  ("sha1", hashlib.sha1),
+	  ("sha256", hashlib.sha256),
+	  ("sha224", hashlib.sha224),
+	  ("sha512", hashlib.sha512),
+	  ("sha384", hashlib.sha384))
 
-print hmac.HMAC(pwd, salt, hashlib.sha1).hexdigest()
-print hmac.HMAC(pwd, salt1, hashlib.sha1).hexdigest()
-print hmac.HMAC(pwd, salt, hashlib.sha512).hexdigest()
 
+alg = sys.argv[1]
+pwd = sys.argv[2]
+#salt1 = salt + "\0\0\0\x01"
+algo = None
+
+for (anm, aob) in algtbl:
+	if alg == anm:
+		algo = aob
+		break
+
+if not algo:
+	print >>sys.stderr, "Hash algorithm %s not found!" % alg
+	sys.exit(2)
+
+hmf = open("HMACS.%s" % alg, "w")
+for fnm in sys.argv[3:]:
+	f = file(fnm, "rb")
+	if not f:
+		print >>sys.stderr, "Could not open %s" % fnm
+		sys.exit(3)
+	print fnm,
+	fcont = f.read()
+	hm = hmac.HMAC(pwd, fcont, algo)
+	print >>hmf, "%s *%s" % (hm.hexdigest(), fnm)
 
