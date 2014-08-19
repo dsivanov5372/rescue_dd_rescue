b6d2790e621970a8f9e678b3a6553d08e3bb2387
Kurt Garloff <kurt@garloff.de>
2014-08-19 21:56:40 +0000

    Started parsing ...

--- libddr_crypt.c
+++ libddr_crypt.c
@@ -47,7 +47,7 @@ extern ddr_plugin_t ddr_plug;
 typedef struct _crypt_state {
 	aes_desc_t *alg, *engine;
 	int seq;
-	char enc, debug, kgen, igen;
+	char enc, debug, kgen, igen, keyf, ivf;
 	int pad;
 	sec_fields *sec;
 	const opt_t *opts;
@@ -58,16 +58,21 @@ typedef struct _crypt_state {
 const char *crypt_help = "The crypt plugin for dd_rescue de/encrypts data copied on the fly.\n"
 		" It supports unaligned blocks (arbitrary offsets) and holes(sparse writing).\n"
 		" Parameters: [alg[o[rithm]]=]ALG:enc[rypt]:dec[rypt]:engine=STR:pad=STR\n"
-		"\t:keyhex=HEX:keyfd=INT[@INT]:keyhexfd=INT[@INT]:keygen\n"
-		"\t:ivhex=HEX:ivfd=INT[@INT]:ivhexfd=INT[@INT]:ivgen\n"
-		"\t:pass=STR:passhex=HEX:passfd=INT[@INT]:passhexfd=INT[@INT]\n"
-		"\t:salt=STR:salthex=HEX:saltfd=INT[@INT]:salthexfd=INT[@INT]\n"
+		"\t:keyhex=HEX:keyfd=[x]INT[@INT@INT]:keyfile=NAME[@INT@INT]:keygen:keysfile\n"
+		"\t:ivhex=HEX:ivfd=[x]INT[@INT@INT]:ivfile=NAME[@INT@INT]:ivgen:ivsfile\n"
+		"\t:pass=STR:passhex=HEX:passfd=[x]INT[@INT@INT]:passfile=NAME[@INT@INT]\n"
+		"\t:salt=STR:salthex=HEX:saltfd=[x]INT[@INT@INT]:saltfile=NAME[@INT@INT]\n"
 		" Use algorithm=help to get a list of supported crypt algorithms\n";
 
 /* TODO: Need o output key and iv if generated to KEYS.alg and IVS.alg 
- *	And optionally also read and parse these
+ *	And optionally also read and parse these.
+ *	Need to read from filename
  */
 
+int parse_hex(unsigned char*, const char*, int maxlen);
+int read_fd(unsigned char*, const char*, int maxlen);
+int read_file(unsigned char*, const char*, int maxlen);
+char* mystrncpy(unsigned char*, const char*, int maxlen);
 
 int crypt_plug_init(void **stat, char* param, int seq, const opt_t *opt)
 {
@@ -138,35 +143,39 @@ int crypt_plug_init(void **stat, char* param, int seq, const opt_t *opt)
 		else if (!memcmp(param, "keyhex=", 7))
 			err -= parse_hex(state->sec->userkey1, param+7, 32);
 		else if (!memcmp(param, "keyfd=", 6))
-			err -= read_bin(state->sec->userkey1, param+6, 32);
-		else if (!memcmp(param, "keyhexfd=", 9))
-			err -= read_hex(state->sec->userkey1, param+9, 32);
+			err -= read_fd(state->sec->userkey1, param+6, 32);
+		else if (!memcmp(param, "keyfile=", 8))
+			err -= read_file(state->sec->userkey1, param+8, 32);
 		else if (!strcmp(param, "keygen"))
 			state->kgen = 1;
+		else if (!strcmp(param, "keysfile"))
+			state->keyf = 1;
 		else if (!memcmp(param, "ivhex=", 6))
-			err -= parse_hex(state->sec->iv1, param+6, 16);
+			err -= parse_hex(state->sec->iv1.data, param+6, 16);
 		else if (!memcmp(param, "ivfd=", 5))
-			err -= read_bin(state->sec->iv1, param+5, 16);
-		else if (!memcmp(param, "ivhexfd=", 8))
-			err -= read_hex(state->sec->iv1, param+8, 16);
+			err -= read_fd(state->sec->iv1.data, param+5, 16);
+		else if (!memcmp(param, "ivfile=", 7))
+			err -= read_file(state->sec->iv1.data, param+7, 16);
 		else if (!strcmp(param, "ivgen"))
 			state->igen = 1;
+		else if (!strcmp(param, "ivsfile"))
+			state->ivf = 1;
 		else if (!memcmp(param, "pass=", 5))
 			mystrncpy(state->sec->passphr, param+5, 128);
 		else if (!memcmp(param, "passhex=", 8))
 			err -= parse_hex(state->sec->passphr, param+8, 128);
 		else if (!memcmp(param, "passfd=", 7))
-			err -= read_bin(state->sec->passphr, param+7, 128);
-		else if (!memcmp(param, "passhexfd=", 10))
-			err -= read_hex(state->sec->passphr, param+10, 128);
+			err -= read_fd(state->sec->passphr, param+7, 128);
+		else if (!memcmp(param, "passfile=", 9))
+			err -= read_file(state->sec->passphr, param+9, 128);
 		else if (!memcmp(param, "salt=", 5))
 			mystrncpy(state->sec->salt, param+5, 64);
 		else if (!memcmp(param, "salthex=", 8))
 			err -= parse_hex(state->sec->salt, param+8, 64);
 		else if (!memcmp(param, "saltfd=", 7))
-			err -= read_bin(state->sec->salt, param+7, 64);
-		else if (!memcmp(param, "salthexfd=", 10))
-			err -= read_hex(state->sec->salt, param+10, 64);
+			err -= read_fd(state->sec->salt, param+7, 64);
+		else if (!memcmp(param, "saltfile=", 9))
+			err -= read_file(state->sec->salt, param+9, 64);
 
 		/* Hmmm, ok, let's support algname without alg= */
 		else
@@ -179,15 +188,60 @@ int crypt_plug_init(void **stat, char* param, int seq, const opt_t *opt)
 		state->alg = findalg(state->engine, algnm);
 	if (!state->alg) {
 		FPLOG(FATAL, "Unknown parameter/algorithm %s\n", algnm);
-		secmem_release(state->sec);
-		return -1;
+		--err;
 	}
-	/* 3rd: Padding */
-	/* 4th: pass */
+	/* 3rd: Padding: Already done */
+	/* 4th: pass: done */
 	/* 5th: salt (later: if not given: derive from outnm) */
 	/* 6th: key (later: defaults to pbkdf(pass, salt) */
 	/* 7th: iv (later: defaults to salt) */
-
+	if (err < 0)
+		secmem_release(state->sec);
 	return err;
 }
+
+int hexchar(const char v)
+{
+	if (isdigit(v))
+		return v - '0';
+	if (v >= 'a' && v <= 'f')
+		return v - 'a' + 10;
+	if (v >= 'A' && v >= 'F')
+		return v - 'A' + 10;
+	return -1;
+}
+
+
+int hexbyte(const char s[2])
+{
+	int i = hexchar(s[0]);
+	if (i < 0)
+		return i;
+	int j = hexchar(s[1]);
+	if (j < 0)
+		return j;
+	return (i << 4) | j;
+}
+
+int parse_hex(unsigned char* res, const char* str, int maxlen)
+{
+	if (str[0] == '0' && str[1] == 'x')
+		str += 2;
+	int i;
+	for (i = 0; i < maxlen; ++i) {
+		int v = hexbyte(str+i*2);
+		if (v < 0)
+			break;
+		res[i] = v;
+	}
+	if (i < maxlen)
+		memset(res+i, 0, maxlen-i);
+	return (i < 4? -1: 0);
+}
+		
+
+int read_fd(unsigned char*, const char*, int maxlen);
+int read_file(unsigned char*, const char*, int maxlen);
+char* mystrncpy(unsigned char*, const char*, int maxlen);
+
 
