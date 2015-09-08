edf9c6471b123cd4489bcf9e2888a99baa27fb3b
Kurt Garloff <kurt@garloff.de>
2015-09-08 23:37:48 +0200

    Try to provide openSSL compat pbkdf on big-endian.

--- pbkdf_ossl.c
+++ pbkdf_ossl.c
@@ -11,8 +11,33 @@
 #include "md5.h"
 #include <stdlib.h>
 #include <assert.h>
+#include <endian.h>
+#include <stdint.h>
 
 #define MIN(a,b) ((a)<(b)? (a): (b))
+
+#if __BYTE_ORDER == __BIG_ENDIAN
+static inline void to_bytes(uint32_t val, uint8_t *bytes)
+{
+	bytes[0] = (uint8_t)val;
+	bytes[1] = (uint8_t)(val >> 8);
+	bytes[2] = (uint8_t)(val >> 16);
+	bytes[3] = (uint8_t)(val >> 24);
+}
+static inline void memcpy_hash(uint8_t *buf, hash_t *hv, size_t hln)
+{
+	int i;
+	assert(hln%sizeof(uint32_t) == 0);
+	for (i=0; i<hln/sizeof(uint32_t); ++i)
+		to_bytes(hv->sha256_h[i], buf+i*sizeof(uint32_t));
+}
+#else
+static inline void memcpy_hash(uint8_t *buf, hash_t *hv, size_t hln)
+{
+	memcpy(buf, hv, hln);
+}
+
+#endif
 
 int pbkdf_ossl(hashalg_t *hash, unsigned char* pwd,  int plen,
 				unsigned char* salt, int slen,
@@ -33,7 +58,7 @@ int pbkdf_ossl(hashalg_t *hash, unsigned char* pwd,  int plen,
 				memcpy(hbuf+plen, salt, slen);
 		} else {
 			hbln += hash->hashln;
-			memcpy(hbuf, &hv, hash->hashln);
+			memcpy_hash(hbuf, &hv, hash->hashln);
 			memcpy(hbuf+hash->hashln, pwd, plen);
 			if (slen)
 				memcpy(hbuf+hash->hashln+plen, salt, slen);
@@ -43,12 +68,12 @@ int pbkdf_ossl(hashalg_t *hash, unsigned char* pwd,  int plen,
 		hash->hash_calc(hbuf, hbln, hbln, &hv);
 		/* Fill in result */
 		if (off+hash->hashln < klen)		
-			memcpy(key+off, &hv, hash->hashln);
+			memcpy_hash(key+off, &hv, hash->hashln);
 		else if (off >= klen)
-			memcpy(iv+off-klen, &hv, MIN(hash->hashln, ivlen+klen-off));
+			memcpy_hash(iv+off-klen, &hv, MIN(hash->hashln, ivlen+klen-off));
 		else {
-			memcpy(key+off, &hv, klen-off);
-			memcpy(iv, (unsigned char*)&hv+klen-off, MIN(hash->hashln-klen+off, ivlen));
+			memcpy_hash(key+off, &hv, klen-off);
+			memcpy_hash(iv, (hash_t*)(((unsigned char*)&hv)+klen-off), MIN(hash->hashln-klen+off, ivlen));
 		}
 		off += hash->hashln;
 		++cnt;
