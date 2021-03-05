657f73adeb41efc9b4d2161569e8217a616f3346
Kurt Garloff <kurt@garloff.de>
2021-03-05 17:35:30 +0100

    Output all calls in test_crypt.sh
    Also add # in front of sections to simplify c'n'p.
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- test_crypt.sh
+++ test_crypt.sh
@@ -7,7 +7,7 @@ enc_dec_compare()
 	if test -n "$4"; then othargs=":$4"; else unset othargs; fi
 	if test -n "$5"; then eng=":engine=$5"; else unset eng; fi
 	if test -n "$6"; then opt="$6"; else opt="-qptA"; fi
-	echo "Validating enc/decryption $eng $alg $othargs"
+	echo "#Validating enc/decryption $eng $alg $othargs"
 	cp -p $file.enc $file.enc.old 2>/dev/null
 	echo $VG ./dd_rescue $opt -L ./libddr_crypt.so=enc$eng:weakrnd:alg=$alg:$keyargs$othargs $file $file.enc 
 	$VG ./dd_rescue $opt -L ./libddr_crypt.so=enc$eng:weakrnd:alg=$alg:$keyargs$othargs $file $file.enc || exit 1
@@ -21,6 +21,13 @@ enc_dec_compare_keys()
 	enc_dec_compare "$1" "$2" "$3" "$4:keysfile:ivsfile" "$5" "$6"
 }
 
+run_ddr()
+{
+	echo $VG ./dd_rescue "$@"
+	$VG ./dd_rescue "$@"
+}
+
+
 ECB_ALGS="AES192-ECB AES192+-ECB AES192x2-ECB"
 CBC_ALGS="AES192-CBC AES192+-CBC AES192x2-CBC"
 CTR_ALGS="AES128-CTR AES128+-CTR AES128x2-CTR AES192-CTR AES192+-CTR AES192x2-CTR AES256-CTR AES256+-CTR AES256x2-CTR"
@@ -30,58 +37,60 @@ else
   TESTALGS="$ECB_ALGS $CBC_ALGS $CTR_ALGS"
 fi
 
-echo "We will eat a lot of entropy ... hopefully you have some left afterwards!"
+echo "#We will eat a lot of entropy ... hopefully you have some left afterwards!"
 
 # MAIN TEST
 if test -e test_aes; then
   LOG=test_aes.log
-  for ALG in $TESTALGS; do echo test_aes $ALG 10000; $VG ./test_aes $ALG 10000 >$LOG 2>&1; if test $? != 0; then cat $LOG; echo "ERROR"; exit 1; fi; done
+  for ALG in $TESTALGS; do echo $VG ./test_aes $ALG 10000; $VG ./test_aes $ALG 10000 >$LOG 2>&1; if test $? != 0; then cat $LOG; echo "ERROR"; exit 1; fi; done
   rm $LOG
 fi
 # Reverse (CTR, ECB)
-echo "*** Reverse ***"
+echo "# *** Reverse ***"
 enc_dec_compare_keys dd_rescue AES192-CTR keygen:ivgen "" "" "-qptAr"
 enc_dec_compare_keys dd_rescue AES192-ECB keygen:ivgen "" "" "-qptAr"
 # Appending (CTR, ECB only when block-aligned)
 enc_dec_compare_keys dd_rescue AES192-CTR
-./dd_rescue -qAx -L ./libddr_crypt.so=enc:weakrnd:alg=AES192-CTR:keysfile:ivsfile dd_rescue dd_rescue.enc || exit 1
+run_ddr -qAx -L ./libddr_crypt.so=enc:weakrnd:alg=AES192-CTR:keysfile:ivsfile dd_rescue dd_rescue.enc || exit 1
 cat dd_rescue dd_rescue > dd_rescue2
-./dd_rescue -qAp -L ./libddr_crypt.so=dec:weakrnd:alg=AES192-CTR:keysfile:ivsfile dd_rescue.enc dd_rescue.cmp || exit 2
+run_ddr -qAp -L ./libddr_crypt.so=dec:weakrnd:alg=AES192-CTR:keysfile:ivsfile dd_rescue.enc dd_rescue.cmp || exit 2
 cmp dd_rescue.cmp dd_rescue2 || exit 3
 rm dd_rescue2
 
 # Holes (all), skiphole
-echo "*** Holes ***"
+echo "# *** Holes ***"
 # Produce file that consists of dd_rescue + hole + dd_rescue
-./dd_rescue -qpt dd_rescue dd_rescue3
-./dd_rescue -qS 512k dd_rescue dd_rescue3
+run_ddr -qpt dd_rescue dd_rescue3
+run_ddr -qS 512k dd_rescue dd_rescue3
 # Ensure there is a hole even if dd_rescue is long
-./dd_rescue -qS 384k -m 128k /dev/zero dd_rescue3
+run_ddr -qS 384k -m 128k /dev/zero dd_rescue3
 # Test without and with skiphole
 enc_dec_compare_keys dd_rescue3 AES192-CTR keygen:ivgen "" "" "-qpt"
 enc_dec_compare_keys dd_rescue3 AES192-CTR keygen:ivgen skiphole "" "-qpt"
 # Store 384k-512k in cmp3
-./dd_rescue -qt -s 384k -m 128k -S 0 dd_rescue3.cmp dd_rescue3.cmp3
+run_ddr -qt -s 384k -m 128k -S 0 dd_rescue3.cmp dd_rescue3.cmp3
 # Should be 128k of zeroes
-./dd_rescue -qm 128k /dev/zero dd_rescue3.cmp2
+run_ddr -qm 128k /dev/zero dd_rescue3.cmp2
 cmp dd_rescue3.cmp2 dd_rescue3.cmp3 || exit 4
 # Repeat test with reverse
 enc_dec_compare_keys dd_rescue3 AES192-CTR keygen:ivgen "" "" "-qptr"
 enc_dec_compare_keys dd_rescue3 AES192-CTR keygen:ivgen skiphole "" "-qptr"
-./dd_rescue -qt -s 384k -m 128k -S 0 dd_rescue3.cmp dd_rescue3.cmp3
+run_ddr -qt -s 384k -m 128k -S 0 dd_rescue3.cmp dd_rescue3.cmp3
 cmp dd_rescue3.cmp2 dd_rescue3.cmp3 || exit 4
+
+#exit 1
 
 # Chain with lzo, hash (all)
 if test "$HAVE_LZO" = "1"; then
-echo "*** Plugin chains ... ***"
+echo "# *** Plugin chains ... ***"
 SHA256SUM=`type -p sha256sum`
-$VG ./dd_rescue -pqt -L ./libddr_hash.so=sha256:outnm=,./libddr_lzo.so=compr,./libddr_hash.so=sha256:output,./libddr_crypt.so=enc:AES192-CTR:keygen:ivgen:weakrnd:keysfile:ivsfile,./libddr_hash.so=sha256:outnm= dd_rescue3 dd_rescue3.enc || exit 1
+run_ddr -pqt -L ./libddr_hash.so=sha256:outnm=,./libddr_lzo.so=compr,./libddr_hash.so=sha256:output,./libddr_crypt.so=enc:AES192-CTR:keygen:ivgen:weakrnd:keysfile:ivsfile,./libddr_hash.so=sha256:outnm= dd_rescue3 dd_rescue3.enc || exit 1
 if test -n "$SHA256SUM"; then
 sha256sum -c CHECKSUMS.sha256 || exit 4
 else
 echo "WARNING: Cant run sha256sum, binary not found"
 fi
-$VG ./dd_rescue -pqt -L ./libddr_hash.so=sha256:chknm,./libddr_crypt.so=AES192-CTR:weakrnd:dec:keysfile:ivsfile,./libddr_lzo.so=decompr,./libddr_hash.so=sha256:outnm dd_rescue3.enc dd_rescue3.cmp
+run_ddr -pqt -L ./libddr_hash.so=sha256:chknm,./libddr_crypt.so=AES192-CTR:weakrnd:dec:keysfile:ivsfile,./libddr_lzo.so=decompr,./libddr_hash.so=sha256:outnm dd_rescue3.enc dd_rescue3.cmp
 if test -n "$SHA256SUM"; then
 sha256sum -c CHECKSUMS.sha256 || exit 4
 fi
@@ -93,7 +102,7 @@ rm -f dd_rescue3 dd_rescue3.enc dd_rescue3.enc.old dd_rescue3.cmp dd_rescue3.cmp
 # Various ways to pass in keys/IVs
 
 # Padding variations
-$VG ./dd_rescue -t -m 4100 /dev/urandom . || exit 1
+run_ddr -t -m 4100 /dev/urandom . || exit 1
 enc_dec_compare_keys urandom AES192-CBC keygen:ivgen pad=always "" "-qpt"
 enc_dec_compare_keys urandom AES192-CBC "" pad=asneeded "" "-qpt"
 # For odd sizes, always and asneeded should be identical
@@ -106,10 +115,10 @@ enc_dec_compare_keys urandom AES192-ECB "" pad=asneeded "" "-qptr"
 # For odd sizes, always and asneeded should be identical
 cmp urandom.enc urandom.enc.old || exit 4
 # Block aligned ("even")
-$VG ./dd_rescue -t -m 4096 urandom urandom.new || exit 1
+run_ddr -t -m 4096 urandom urandom.new || exit 1
 # Ensure that we don't have 01 or 02 02 or ... at the end,
 # which would trip pad=asneeded
-echo -n "a" | $VG ./dd_rescue -S 4095 -m 1 - urandom.new
+echo -n "a" | run_ddr -S 4095 -m 1 - urandom.new
 mv urandom.new urandom
 enc_dec_compare_keys urandom AES192-CBC "" pad=always "" "-qpt"
 enc_dec_compare_keys urandom AES192-CBC "" pad=asneeded "" "-qpt"
@@ -130,7 +139,7 @@ rm -f urandom urandom.enc urandom.enc.old urandom.cmp
 
 # OpenSSL compatibility
 
-echo "*** OpenSSL compatibility ***"
+echo "# *** OpenSSL compatibility ***"
 if openssl enc -aes-192-ctr -K 4d20e517cd98ff130ac160dcb4177ef1ab4e8f9501bc6e1d -iv f61059ec2d87a410853b8f1500dead00 -in dd_rescue -out dd_rescue.enc.o; then
   enc_dec_compare dd_rescue AES192-CTR "" keyhex=4d20e517cd98ff130ac160dcb4177ef1ab4e8f9501bc6e1d:ivhex=f61059ec2d87a410853b8f1500dead00
   cmp dd_rescue.enc dd_rescue.enc.o || exit 4
@@ -161,10 +170,10 @@ if openssl enc -aes-192-ctr -K 4d20e517cd98ff130ac160dcb4177ef1ab4e8f9501bc6e1d 
 fi
 
 
-echo "*** Algorithms ... ***"
+echo "# *** Algorithms ... ***"
 # Algs and Engines
 for alg in $TESTALGS; do
-	echo "** $alg **"
+	echo "# ** $alg **"
 	# Generate key+IV, save to index file and use for decryption
 	enc_dec_compare_keys dd_rescue $alg keygen:ivgen
 	## Generate key+IV, save to binary files 
@@ -174,7 +183,7 @@ for alg in $TESTALGS; do
 	# Use random numbers and write to index file
 	enc_dec_compare dd_rescue $alg saltgen pass=PAWD:pbkdf2:saltsfile
 done
-echo "*** Salt and XAttrs ***"
+echo "# *** Salt and XAttrs ***"
 # Use random numbers and write to binary file
 enc_dec_compare dd_rescue AES192-CTR saltgen pass=PWD_:pbkdf2:saltfile=SALT
 if grep 'define HAVE_\(SYS\|ATTR\)_XATTR_H 1' config.h >/dev/null 2>&1 ; then
@@ -188,7 +197,7 @@ HAVE_AESNI=`grep " sse4" /proc/cpuinfo 2>/dev/null | grep " aes " 2>/dev/null`
 HAVE_AESARM=`grep " pmull " /proc/cpuinfo 2>/dev/null`
 HAVE_LIBCRYPTO=`grep 'HAVE_LIBCRYPTO 1' config.h 2>/dev/null`
 if test -n "$TESTALGS"; then
-  echo "*** Engines comparison ***"
+  echo "# *** Engines comparison ***"
 fi
 for alg in $TESTALGS; do
 	rm dd_rescue.enc.old dd_rescue.enc
@@ -207,7 +216,7 @@ for alg in $TESTALGS; do
 	fi
 	if test -z "$HAVE_LIBCRYPTO"; then ENG=`echo $ENG | sed 's/ openssl//'`; fi
 	if test "$HAVE_AES" = "0"; then ENG=`echo $ENG | sed 's/ aesni//'`; fi
-	echo "** Alg $alg engines $ENG **"
+	echo "#** Alg $alg engines $ENG **"
 	for engine in $ENG; do
 		enc_dec_compare dd_rescue $alg "" pass=PASSWORD:pbkdf2 $engine
 		if test -e dd_rescue.enc.old; then cmp dd_rescue.enc dd_rescue.enc.old || exit 4; fi
