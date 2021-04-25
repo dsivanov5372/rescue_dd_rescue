131a84f32e496ce5e19d51bf308329639bb369da
Kurt Garloff <kurt@garloff.de>
2021-04-25 11:27:09 +0200

    Fewer repetitions. Big test is 15k instead of 16k.
    The latter to optimize for 32k DCache ...
    
    Signed-off-by: Kurt Garloff <kurt@garloff.de>

--- test_crypt.sh
+++ test_crypt.sh
@@ -57,10 +57,10 @@ if test -e test_aes; then
   LOG=test_aes.log
   #for ALG in $TESTALGS; do echo $VG ./test_aes $ALG 10000; $VG ./test_aes $ALG 10000 >$LOG 2>&1; if test $? != 0; then cat $LOG; echo "ERROR"; exit 1; fi; done
   #rm $LOG
-  echo $VG test_aes AES128-CTR 10000 0 16400
-  $VG ./test_aes AES128-CTR 10000 0 16400
+  echo $VG test_aes AES128-CTR 10000 0 15392
+  $VG ./test_aes AES128-CTR 10000 0 15392
   for len in $(seq 0 130); do
-    for ALG in $TESTALGS; do echo $VG ./test_aes $ALG 100 0 $len; $VG ./test_aes $ALG 100 0 $len >$LOG 2>&1; if test $? != 0; then cat $LOG; echo "ERROR"; exit 1; fi; done
+    for ALG in $TESTALGS; do echo $VG ./test_aes $ALG 10 0 $len; $VG ./test_aes $ALG 10 0 $len >$LOG 2>&1; if test $? != 0; then cat $LOG; echo "ERROR"; exit 1; fi; done
   done
   rm $LOG
 fi
