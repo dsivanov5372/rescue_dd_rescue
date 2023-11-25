924bdaf622bf02592f34a31b60e312eba4a779fd
dsivanov5372 <mitya5372@yandex.ru>
2023-11-25 17:43:11 +0300

    add llzma library check

--- libddr_lzma.c
+++ libddr_lzma.c
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
-#include "lzma.h"
+#include <lzma.h>
 
 #define CHUNK_SIZE 4096
 
@@ -47,74 +47,74 @@ const char* null_help = "LZMA plugin which is doing compression/decompression fo
 lzma_ret init_lzma_stream(lzma_state* state) {
     // disable limits, no check done for uncompressed data
     return state->mode == COMPRESS ? lzma_easy_encoder(&(state->strm), 6, LZMA_CHECK_NONE) :
-           lzma_auto_decoder(&(state->strm), UINT64_MAX, LZMA_CONCATENATED | LZMA_TELL_UNSUPPORTED_CHECK);
+            lzma_auto_decoder(&(state->strm), UINT64_MAX, LZMA_CONCATENATED | LZMA_TELL_UNSUPPORTED_CHECK);
 }
 
 int lzma_plug_init(void **stat, char* param, int seq, const opt_t *opt)
 {
-	lzma_state *state = (lzma_state *)malloc(sizeof(lzma_state));
-	*stat = (void *)state;
-	memset(state, 0, sizeof(lzma_state));
+    lzma_state *state = (lzma_state *)malloc(sizeof(lzma_state));
+    *stat = (void *)state;
+    memset(state, 0, sizeof(lzma_state));
     lzma_stream strm = LZMA_STREAM_INIT;
     state->strm = strm;
-	state->seq = seq;
+    state->seq = seq;
 
-	while (param) {
-		char* next = strchr(param, ':');
-		if (next) {
+    while (param) {
+        char* next = strchr(param, ':');
+        if (next) {
             *next++ = 0;
         }
 
-		if (!strcmp(param, "help")) {
+        if (!strcmp(param, "help")) {
             FPLOG(INFO, "%s", null_help);
         } else if (!strcmp(param, "z")) {
             state->mode = COMPRESS;
         } else if (!strcmp(param, "d")) {
             state->mode = DECOMPRESS;
         } else {
-			FPLOG(FATAL, "plugin doesn't understand param %s\n", param);
-			return 1;
-		}
-		param = next;
-	}
-	return 0;
+            FPLOG(FATAL, "plugin doesn't understand param %s\n", param);
+            return 1;
+        }
+        param = next;
+    }
+    return 0;
 }
 
 int lzma_plug_release(void **stat)
 {
-	if (!stat || !*stat)
-		return -1;
+    if (!stat || !*stat)
+        return -1;
 
-	lzma_state *state = (lzma_state *)*stat;
+    lzma_state *state = (lzma_state *)*stat;
     free(state->output);
 
-	free(*stat);
-	return 0;
+    free(*stat);
+    return 0;
 }
 
 int lzma_open(const opt_t *opt, int ilnchg, int olnchg, int ichg, int ochg,
-	      unsigned int totslack_pre, unsigned int totslack_post,
+          unsigned int totslack_pre, unsigned int totslack_post,
 	      const fstate_t *fst, void **stat)
 {
     lzma_state *state = (lzma_state*)*stat;
-	state->opts = opt;
+    state->opts = opt;
 
-	if (state->mode == AUTO) {
-		if (!strcmp(opt->iname + strlen(opt->iname) - 2, "xz"))
-			state->mode = DECOMPRESS;
-		else if (!strcmp(opt->oname + strlen(opt->oname) - 2, "xz"))
-			state->mode = COMPRESS;
-		else {
-			FPLOG(FATAL, "can't determine compression/decompression from filenames (and not set)!\n");
-			return -1;
-		}
-	}
+    if (state->mode == AUTO) {
+        if (!strcmp(opt->iname + strlen(opt->iname) - 2, "xz"))
+            state->mode = DECOMPRESS;
+        else if (!strcmp(opt->oname + strlen(opt->oname) - 2, "xz"))
+            state->mode = COMPRESS;
+        else {
+            FPLOG(FATAL, "can't determine compression/decompression from filenames (and not set)!\n");
+            return -1;
+        }
+    }
 
     if (init_lzma_stream(state) != LZMA_OK) {
-		FPLOG(FATAL, "failed to initialize lzma library!");
-		return -1;
-	}
-	return 0;
+        FPLOG(FATAL, "failed to initialize lzma library!");
+        return -1;
+    }
+    return 0;
 }
 
 #if __WORDSIZE == 64
@@ -152,7 +152,7 @@ unsigned char* lzma_algo(unsigned char *bf, lzma_state *state, int eof, fstate_t
     }
     size_t bf_len = malloc_usable_size(bf);
     size_t real_len = (state->file_size - state->curr_pos) > bf_len ?
-        bf_len : (state->file_size - state->curr_pos);
+            bf_len : (state->file_size - state->curr_pos);
 
     if (state->output == NULL) {
         state->buf_len = (bf_len > CHUNK_SIZE ? bf_len * 2 : CHUNK_SIZE * 2) + 1;
@@ -203,7 +203,7 @@ unsigned char* lzma_algo(unsigned char *bf, lzma_state *state, int eof, fstate_t
 unsigned char* lzma_compress(fstate_t *fst, unsigned char *bf, 
 			    int *towr, int eof, int *recall, lzma_state *state)
 {
-   return lzma_algo(bf, state, eof, fst, towr);
+    return lzma_algo(bf, state, eof, fst, towr);
 }
 
 unsigned char* lzma_decompress(fstate_t *fst, unsigned char* bf, int *towr,
@@ -215,45 +215,45 @@ unsigned char* lzma_decompress(fstate_t *fst, unsigned char* bf, int *towr,
 unsigned char* lzma_blk_cb(fstate_t *fst, unsigned char* bf, 
 			   int *towr, int eof, int *recall, void **stat)
 {
-	lzma_state *state = (lzma_state*)*stat;
+    lzma_state *state = (lzma_state*)*stat;
 
-	unsigned char* ptr = 0;	/* Silence gcc */
-	clock_t t1 = 0;
-	if (state->do_bench) {
-		t1 = clock();
+    unsigned char* ptr = 0;	/* Silence gcc */
+    clock_t t1 = 0;
+    if (state->do_bench) {
+        t1 = clock();
     }
 
-	if (state->mode == COMPRESS) 
-		ptr = lzma_compress(fst, bf, towr, eof, recall, state);
-	else {
-		ptr = lzma_decompress(fst, bf, towr, eof, recall, state);
-	}
-
-	if (state->do_bench) {
-		state->cpu += clock() - t1;
+    if (state->mode == COMPRESS) 
+        ptr = lzma_compress(fst, bf, towr, eof, recall, state);
+    else {
+        ptr = lzma_decompress(fst, bf, towr, eof, recall, state);
     }
-	return ptr;
+
+    if (state->do_bench) {
+        state->cpu += clock() - t1;
+    }
+    return ptr;
 }
 
 int lzma_close(loff_t ooff, void **stat)
 {
     lzma_state *state = (lzma_state *)*stat;
-	/* Only output if it took us more than 0.05s, otherwise it's completely meaningless */
-	if (state->do_bench && state->cpu / (CLOCKS_PER_SEC / 20) > 0) {
+    /* Only output if it took us more than 0.05s, otherwise it's completely meaningless */
+    if (state->do_bench && state->cpu / (CLOCKS_PER_SEC / 20) > 0) {
         FPLOG(INFO, "%.2fs CPU time\n", (double)state->cpu / CLOCKS_PER_SEC);
     }
 
     lzma_end(&(state->strm));
-	return 0;
+    return 0;
 }
 
 ddr_plugin_t ddr_plug = {
-	.name = "lzma",
-	.needs_align = 0,
-	.handles_sparse = 1,
-	.init_callback  = lzma_plug_init,
-	.open_callback  = lzma_open,
-	.block_callback = lzma_blk_cb,
-	.close_callback = lzma_close,
-	.release_callback = lzma_plug_release,
+    .name = "lzma",
+    .needs_align = 0,
+    .handles_sparse = 1,
+    .init_callback  = lzma_plug_init,
+    .open_callback  = lzma_open,
+    .block_callback = lzma_blk_cb,
+    .close_callback = lzma_close,
+    .release_callback = lzma_plug_release,
 };
